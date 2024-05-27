eb2830b8b467c55fdaf90ded4d6efe1c952a5fa4
LidiaSolaGratiaBrTarigan <167161745+LidiaSolaGratiaBrTarigan@users.noreply.github.com>
2024-05-27 18:12:57 +0700

    Implementasi Firebase

---
+++ ContactDao.java
@@ -0,0 +1,17 @@
+package com.example.recycler;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ContactDao {
+    @Query(value = "SELECT * FROM contacts")
+    List<Contact> getAllContacts();
+
+    @Insert
+    void insert(Contact contact);
+
+}
